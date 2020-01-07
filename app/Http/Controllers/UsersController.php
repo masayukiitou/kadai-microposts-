ea12d5cedf2c38452bb38515643d585ed2a9bae3
itou.masayuki <m_asaitou@yahoo.co.jp>
2020-01-07 13:24:32 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,12 +23,14 @@ class UsersController extends Controller
         $user = User::find($id);
         $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
         
-        return view('users.show', [
+        $data = [
             'user' => $user,
             'microposts' => $microposts,
-        ]);
+        ];
         
         $data += $this->counts($user);
+        
+        return view('users.show', $data);
     }
     
     public function followings($id)
